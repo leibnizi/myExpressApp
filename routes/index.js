cfe3cd689bc0cef994aeef5e6fbf17b9bad4bba6
wedge <wedgechiang18@gmail.com>
2018-10-05 00:05:42 +0800

    add nodemon

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  // res.render("index", { title: "myExpress" });
+  // res.json({ user: "tobi" });
+  res.render("index", { title: "MyExpress" }, function(err, html) {
+    // res.send(html);
+  });
 });
 
 module.exports = router;
