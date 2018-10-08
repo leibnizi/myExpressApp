4c880099996fd49568bc6d06a26fdfbd6e355460
wedge <wedgechiang18@gmail.com>
2018-10-08 16:12:36 +0800

    cors

--- app.js
+++ app.js
@@ -22,6 +22,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use("static", express.static(path.join(__dirname, "public")));
 
+var allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+};
+app.use(allowCrossDomain);
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/mongo", mongoRouter);
